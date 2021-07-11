e0e86f0df50632b15c323aafc3eda501f28e3e0b
med amine <amed14170@gmail.com>
2021-07-12 00:34:31 +0100

    initial commit

---
+++ src/Entity/Education.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EducationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EducationRepository::class)
+ */
+class Education
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateD;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateF;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lieu;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDateD(): ?\DateTimeInterface
+    {
+        return $this->dateD;
+    }
+
+    public function setDateD(\DateTimeInterface $dateD): self
+    {
+        $this->dateD = $dateD;
+
+        return $this;
+    }
+
+    public function getDateF(): ?\DateTimeInterface
+    {
+        return $this->dateF;
+    }
+
+    public function setDateF(\DateTimeInterface $dateF): self
+    {
+        $this->dateF = $dateF;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
